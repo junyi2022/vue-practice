e401446bf9ebe7b9e0a12e375a1b77c8cc32e156
junyi2022 <junyi2022@berkeley.edu>
2024-04-12 15:12:41 -0400

    deploy vite test

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-pratice/",
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/vue-pratice/',
 })
